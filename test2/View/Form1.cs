86bcc2db45ad05aef81c5f822309b85fd114e8ea
Lò Văn Anh <anhlo2819@gmail.com>
2024-12-04 16:09:21 +0700

    them
    a

--- test2/View/Form1.cs
+++ test2/View/Form1.cs
@@ -16,5 +16,10 @@ namespace test2
         {
             InitializeComponent();
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
     }
 }
